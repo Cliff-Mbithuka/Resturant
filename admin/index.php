ebc6db0e97407ea9dd14a4775ece53551c88f984
Clifford Musyoka <your_email@example.com>
2024-07-03 17:26:50 +0300

    Add partials

--- admin/index.php
+++ admin/index.php
@@ -1,27 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Food website - Home Page</title>
-    <link rel="stylesheet" href="../css/admin.css">
-</head>
-
-<body>
-    <!----- Menu section startts-->
-    <div class="menu">
-        <div class="wrapper">
-            <ul>
-                <li><a href="#">Home</a></li>
-                <li><a href="#">Admin</a></li>
-                <li><a href="#">Category</a></li>
-                <li><a href="#">Food</a></li>
-                <li><a href="#">Order</a></li>
-            </ul>
-        </div>
-    </div>
-    <!--------- Menu section ends--->
+    <?php 
+    include('partials/menu.php');
+    ?>
+   
 
     <!---Main Content Section starts--->
     <div class="main-content">
@@ -49,15 +30,4 @@
     </div>
     <!---- Main content section Ends--->
 
-    <!-----Footer Section Starts-->
-    <div class="footer">
-        <div class="wrapper">
-            <p class="text-center">2020 All rights reserved. Some Resturant Developed By - <a href="#">Mbithuka Clifford</a></p>
-
-        </div>
-    </div>
-    <!-----Footer Section ends-->
-
-</body>
-
-</html>
+    <?php include('partials/footer.php') ?>
---
+++ admin/partials/footer.php
@@ -0,0 +1,12 @@
+<!-----Footer Section Starts-->
+<div class="footer">
+        <div class="wrapper">
+            <p class="text-center">2020 All rights reserved. Some Resturant Developed By - <a href="#">Mbithuka Clifford</a></p>
+
+        </div>
+    </div>
+    <!-----Footer Section ends-->
+
+</body>
+
+</html>
