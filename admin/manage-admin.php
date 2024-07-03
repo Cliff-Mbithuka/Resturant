983f1f91e5b2e3afc9a10c2047da8fbf058ac7ce
Clifford Musyoka <your_email@example.com>
2024-07-03 15:54:00 +0300

    first commit

---
+++ admin/manage-admin.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Food website - Home Page</title>
+    <link rel="stylesheet" href="../css/admin.css">
+</head>
+
+<body>
+    <!----- Menu section startts-->
+    <div class="menu">
+        <div class="wrapper">
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Admin</a></li>
+                <li><a href="#">Category</a></li>
+                <li><a href="#">Food</a></li>
+                <li><a href="#">Order</a></li>
+            </ul>
+        </div>
+    </div>
+    <!--------- Menu section ends--->
+
+    <!---Main Content Section starts--->
+    <div class="main-content">
+        <div class="wrapper">
+            <h1>DASHBORD</h1>
+
+            <div class="col-4 text-center">
+                <h1>5</h1>
+                Categories
+            </div>
+            <div class="col-4 text-center">
+                <h1>5</h1>
+                Categories
+            </div>
+            <div class="col-4 text-center">
+                <h1>5</h1>
+                Categories
+            </div>
+            <div class="col-4 text-center">
+                <h1>5</h1>
+                Categories
+            </div>
+            <div class="clear-fix"></div>
+        </div>
+    </div>
+    <!---- Main content section Ends--->
+
+    <!-----Footer Section Starts-->
+    <div class="footer">
+        <div class="wrapper">
+            <p class="text-center">2020 All rights reserved. Some Resturant Developed By - <a href="#">Mbithuka Clifford</a></p>
+
+        </div>
+    </div>
+    <!-----Footer Section ends-->
+
+</body>
+
+</html>
